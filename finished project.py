3bc14931e48f94455374177a4160de5390e8d9d2
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2019-03-16 11:31:30 +0000

    the main python file

---
+++ finished project.py
@@ -0,0 +1,23 @@
+# Add your Python code here. E.g.
+from microbit import *
+import speech
+speech.say("ready")
+while True:
+
+    #Checks how much current travels from pin1 to ground
+    #If nobody is holding on to pin1 and ground this number will be
+    #lower. When somebody is holding this pin (and is conducting the
+    #current) then this value will be higher)
+    threshold = pin1.read_analog()
+    #If the value of threshold is over 300
+    if threshold < 100:
+        #Say hello
+        speech.say("Hello!")
+        '''I used this next bit to check if the happy face kept repeating
+        itself'''
+        #Show smiley face for 1 second
+        display.show(Image.HAPPY)
+        sleep(1000)
+        #Clear the display
+        display.clear()
+
